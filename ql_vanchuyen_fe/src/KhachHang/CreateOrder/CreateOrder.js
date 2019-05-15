e79573477bc038bf1db8963c57694b14d2ce3714
bpbn <bpbngoc11@gmail.com>
2019-05-15 08:58:08 +0700

    load QLTaiXe

--- ql_vanchuyen_fe/src/KhachHang/CreateOrder/CreateOrder.js
+++ ql_vanchuyen_fe/src/KhachHang/CreateOrder/CreateOrder.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { memo, useState } from 'react';
+// import React from 'react';
+// import { memo, useState } from 'react';
 
-const CreateOrder = () => {
+// const CreateOrder = () => {
 
-    const [formState, setFormState] = useState({
-        id, loaiHangHoa, tenHangHoa, 
-    })
+//     const [formState, setFormState] = useState({
+//         id, loaiHangHoa, tenHangHoa, 
+//     })
 
-    return (
-        <div className='createorder_container'>
-            <div className='form_create' style={{backgroundColor: 'white'}}>
-                <form>
-                    <div className='form-create-outline md-4' disabled>
-                        <input type="text" id="idOrder" name="id" value={}/>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
-};
+//     return (
+//         <div className='createorder_container'>
+//             <div className='form_create' style={{backgroundColor: 'white'}}>
+//                 <form>
+//                     <div className='form-create-outline md-4' disabled>
+//                         <input type="text" id="idOrder" name="id" value={}/>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default memo(CreateOrder);
+// export default memo(CreateOrder);
