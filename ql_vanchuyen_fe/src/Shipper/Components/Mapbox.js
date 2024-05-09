9c1e809885cda080ae7d4bddd257dff89a72ea62
ouoctiiai <131683324+ouoctiiai@users.noreply.github.com>
2024-05-09 13:47:42 +0700

    Update Mapbox.js

--- ql_vanchuyen_fe/src/Shipper/Components/Mapbox.js
+++ ql_vanchuyen_fe/src/Shipper/Components/Mapbox.js
@@ -21,19 +21,36 @@ function Mapbox() {
     longitude: 105.8012
   }
 
+  const [locateb, setLocateb] = useState();
   const [distance, setDistance] = useState(null);
   const MAPBOX_TOKEN = 'pk.eyJ1IjoiYnBibiIsImEiOiJjbHZ4cGVkNWwyZm41MmltZ2lpam1oZXE4In0.81EqtEQcONtNnO_l4iwBhQ';
+
+  // Code from the image
+  const getLocate = (address) => {
+    axios
+      .get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${MAPBOX_TOKEN}`
+      )
+      .then(function (responseLocate) {
+        setLocateb({
+          latitude: responseLocate.data.features[0].center[1],
+          longitude: responseLocate.data.features[0].center[0],
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   const getDirections = (origin, destination) => {
     axios
       .get(
-        `https://api.mapbox.com/directions/v5/mapbox/driving/${origin.longitude},${origin.latitude};${destination.longitude},${destination.latitude}.json?access_token=${MAPBOX_TOKEN}&geometries=geojson` // Include geometries parameter
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${origin.longitude},${origin.latitude};${destination.longitude},${destination.latitude}.json?access_token=${MAPBOX_TOKEN}&geometries=geojson`
       )
       .then(function (response) {
         const distance = response.data.routes[0].distance / 1000;
         setDistance(distance);
         const geojson = response.data.routes[0].geometry;
-        // Add logic to update viewport to focus on the route (optional)
       })
       .catch(function (error) {
         console.log(error);
@@ -47,11 +64,18 @@ function Mapbox() {
       onViewportChange={(viewport) => setViewport(viewport)}
       mapboxAccessToken = {MAPBOX_TOKEN}
     >
+      {getLocate("Ha Noi, Vietnam")}
       {getDirections(addressa, addressb)}
 
       {distance && (
         <div>
           Khoảng cách: {distance.toFixed(2)} km
+        </div>
+      )}
+
+      {locateb && (
+        <div>
+          Địa chỉ: {locateb.latitude}, {locateb.longitude}
         </div>
       )}
     </ReactMapGL>
