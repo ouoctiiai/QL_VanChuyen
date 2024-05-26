d41b5405e0cbc093a42ff33a682aa4b50002aeb8
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2024-05-26 20:10:10 +0700

    update header

--- ql_vanchuyen_fe/src/KhachHang/Render/Header.js
+++ ql_vanchuyen_fe/src/KhachHang/Render/Header.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import './Header.scss'
+import { useHistory } from 'react-router-dom';
 
 
 const Header = () => {
+    const history = useHistory();
+
+    const handleButtonHome = () => {
+        history.push("/");
+        window.location.reload(true);
+    }
     return (
         <div className='header_container'>
             <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand">Logo</a>
                     <div class="d-flex">
-                        <button class="btn-header mx-3" type="submit">Home</button>
+                        <button class="btn-header mx-3" type="submit" onClick={handleButtonHome}>Home</button>
                         <button class="btn-header mx-3" type="submit">News</button>
                         <button class="btn-header mx-3" type="submit">About us</button>
                         <button class="btn-header mx-3" type="submit">Contact us</button>
