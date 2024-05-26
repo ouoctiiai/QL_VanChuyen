7082ffa227dd889ce35d766e3e63a590fa008703
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2024-05-26 20:18:47 +0700

    update logo, tên web and logo header

--- ql_vanchuyen_fe/src/KhachHang/Render/Header.js
+++ ql_vanchuyen_fe/src/KhachHang/Render/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
         <div className='header_container'>
             <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand">Logo</a>
+                    <a class="navbar-brand"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Spring_Framework_Logo_2018.svg/2560px-Spring_Framework_Logo_2018.svg.png" /></a>
                     <div class="d-flex">
                         <button class="btn-header mx-3" type="submit" onClick={handleButtonHome}>Home</button>
                         <button class="btn-header mx-3" type="submit">News</button>
